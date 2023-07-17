0ca9e7bc04860b89a0fbb29987e45abdf1e4a0a1
Statfine <309814872@qq.com>
2023-07-17 11:05:16 +0800

    feat: adjust eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
 };
